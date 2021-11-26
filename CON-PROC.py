169ce4cd46024cc7ddf0cace0650c3c40320fd58
Your Name <you@example.com>
2021-11-26 09:25:34 -0300

    se agregaron 2 procedimientos de la base de datos

--- CON-PROC.py
+++ CON-PROC.py
@@ -2,6 +2,34 @@ from typing import Counter
 import mysql.connector
 from mysql.connector import Error
 
+#Buscar producto (si se pasa una tupla se cae xd)
+def buscarprod (datos)  :
+    try :
+        conexion = mysql.connector.connect(
+        host = 'localhost',
+        port = 3306,
+        user = 'root',
+        password = 'admin1234',
+        db = 'todomarket_vip'
+    )
+        if conexion.is_connected() :
+            print("Conexion exitosa.")
+            cursor=conexion.cursor()
+            sentencia = "SELECT * FROM producto WHERE nom='{}'"
+            cursor.execute(sentencia.format(datos))
+            resultados = cursor.fetchall()
+            for fila in resultados  :
+                print(fila[1],fila[2],fila[3],fila[4])
+                
+        else    :
+            print("Dato no encontrado") 
+    except Error as ex :
+        print("Error de conexion", ex)
+    finally :
+        if  conexion.is_connected() :
+            conexion.close() #cierro conexion con la base
+            print("Conexion finalizada.")
+                        
 #Agregar producto
 def agregarprod (datos)  :
     try :
@@ -26,20 +54,7 @@ def agregarprod (datos)  :
             conexion.close() #cierro conexion con la base
             print("Conexion finalizada.")
 
-def datosdeprueba ()    :
-    nombre = "pampita"
-    cantidad = 11
-    codigo_de_barras = 4321
-    precio = 150
-    
-    producto = (nombre, cantidad, codigo_de_barras, precio)
-    return producto
-
-#pruebas
-productos = datosdeprueba()   
-#agregarprod(productos)    
-
-#eliminar producto
+#Eliminar producto
 def eliminarProd (datos)  :
     try :
         conexion = mysql.connector.connect(
@@ -62,7 +77,41 @@ def eliminarProd (datos)  :
         if  conexion.is_connected() :
             conexion.close() #cierro conexion con la base
             print("Conexion finalizada.")
- 
-eliminarProd(productos)
                 
+#Actualizar producto
+def actualizarProd (datos)  :
+    try :
+        conexion = mysql.connector.connect(
+        host = 'localhost',
+        port = 3306,
+        user = 'root',
+        password = 'admin1234',
+        db = 'todomarket_vip'
+    )
+        if conexion.is_connected() :
+            print("conexion exitosa.")
+            cursor=conexion.cursor()
+            sentencia = "UPDATE producto SET nom='{}', cant ={}, prec={} WHERE cod_bar = {}"
+            cursor.execute(sentencia.format(datos[0], datos[1], datos[3], datos[2]))
+            conexion.commit()
+            print("Registro actualizado con exito") 
+    except Error as ex :
+        print("Error de conexion", ex)
+    finally :
+        if  conexion.is_connected() :
+            conexion.close() #cierro conexion con la base
+            print("Conexion finalizada.")
+ 
             
+#PPRUEBAS
+nombre = 'azucar '#'coca cola' #"pampita"
+cantidad = 10 #110
+codigo_de_barras = 55
+precio = 1500 #20000
+    
+producto = (nombre, cantidad, codigo_de_barras, precio) 
+ 
+#agregarprod(producto)  
+#eliminarProd(producto)
+#actualizarProd(producto)
+#buscarprod('azucar') 
